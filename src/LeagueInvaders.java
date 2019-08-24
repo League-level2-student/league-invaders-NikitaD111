83c04b2ef942d757e84fcb16760d5bf9d0a4b645
League Student <league@iMac-20.attlocal.net>
2019-08-23 18:31:28 -0700

    LeagueInavders(ip), GamePanel(ip)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,23 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame gameFrame;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	GamePanel gamePanel = new GamePanel();
+	public static void main(String[] args) {
+		LeagueInvaders leaugeInvaders = new LeagueInvaders();
+		
+	}
+	LeagueInvaders(){
+		gameFrame = new JFrame();
+		setup();
+		
+	}
+	void setup(){
+		gameFrame.add(gamePanel);
+		gameFrame.setSize(WIDTH, HEIGHT);
+		gameFrame.setVisible(true);
+		gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
