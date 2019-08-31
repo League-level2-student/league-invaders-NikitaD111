2992726ff049f673c5cd5b082d20457a4788b910
League Student <league@iMac-27.attlocal.net>
2019-08-30 18:31:10 -0700

    GamePanel(ip)

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
@@ -9,15 +10,23 @@ public class GamePanel extends JPanel {
     final int GAME = 1;
     final int END = 2;
     int currentState = MENU;
+    Font titleFont = new Font("Arial", Font.PLAIN, 48);
+    Font titleFont1 = new Font("Arial", Font.PLAIN, 28);
+    Font titleFont2 = new Font("Arial", Font.PLAIN, 28);
 	@Override
 	public void paintComponent(Graphics g){
 		if(currentState == MENU){
 		    drawMenuState(g);
 		}else if(currentState == GAME){
+			
 		    drawGameState(g);
 		}else if(currentState == END){
+			
 		    drawEndState(g);
 		}
+	}
+	GamePanel(){
+		
 	}
 	void updateMenuState() { 
 		
@@ -31,6 +40,16 @@ public class GamePanel extends JPanel {
 	void drawMenuState(Graphics g) {  
 		g.setColor(Color.BLUE);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.WHITE);
+		g.drawString("League Invaders", 75, 150);
+		g.setFont(titleFont1);
+		g.setColor(Color.WHITE);
+		g.drawString("Press ENTER to start", 100, 450);
+		g.setFont(titleFont2);
+		g.setColor(Color.WHITE);
+		g.drawString("Press SPACE for instructions", 50, 600);
+
 	 }
 	void drawGameState(Graphics g) { 
 		g.setColor(Color.BLACK);
@@ -40,5 +59,9 @@ public class GamePanel extends JPanel {
 	void drawEndState(Graphics g)  {  
 		g.setColor(Color.RED);
 		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
+		g.drawString("GAME OVER", 75, 150);
+
 	 }
 }
