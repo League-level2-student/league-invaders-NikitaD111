8da5e8f48d1740636ef4d04d4e115a6d80665058
League Student <league@iMac-37.attlocal.net>
2019-09-27 18:34:04 -0700

    LeagueInvaders(ip)
    Model Management question 8

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,55 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
+	Rocketship rocket;
+	Random random = new Random();
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+
+	ObjectManager(Rocketship ship) {
+		rocket = ship;
+	}
+
+	void addProjectile(Projectile projectile) {
+		projectiles.add(projectile);
+	}
+
+	void addAlien() {
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+	}
+
+	void update() {
+		rocket.update();
+		for (int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).update();
+		}
+		for (int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).update();
+		}
+	}
+
+	void draw(Graphics g) {
+		rocket.draw(g);
+		for (int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).draw(g);
+		}
+		for (int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).draw(g);
+		}
+	}
+	void purgeObjects() {
+		for (int i = 0; i < aliens.size(); i++) {
+			if (!aliens.get(i).isActive) {
+				aliens.remove(i);
+			}
+		}
+		for (int i = 0; i < projectiles.size(); i++) {
+			if(!projectiles.get(i).isActive) {
+				projectiles.remove(i);
+			}
+		}
+	}
 
 }
